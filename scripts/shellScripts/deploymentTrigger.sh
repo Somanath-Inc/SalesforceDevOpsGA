22c5c2588da86625e35e6f77839d5b53bf17d6b9
Somanathkl <lsomanath@deloitte.com>
2024-03-01 15:24:36 +0530

    Deleted one object

---
+++ scripts/shellScripts/deploymentTrigger.sh
@@ -0,0 +1,37 @@
+if [ $# -ne 1 ];
+then
+    echo "Please provide username"
+    exit 1
+else
+    USERNAME=$1
+fi
+
+if [ -f package/package.xml ];
+then
+    cmpTypeCount_Pkg=$(grep -c "<name>*</name>" package/package.xml)
+    if [ -f package/destructiveChanges.xml ];
+    then
+        cmpTypeCount_dePkg=$(grep -c "<name>*</name>" package/destructiveChanges.xml)
+    fi
+fi
+echo "cmpTypeCount_Pkg : $cmpTypeCount_Pkg --------- cmpTypeCount_dePkg : $cmpTypeCount_dePkg ----------"
+if [ $cmpTypeCount_Pkg -gt 0 ] || [ $cmpTypeCount_dePkg -gt 0 ]
+then
+    echo "Started Deployment ---------------- "
+    sfdx force:source:deploy --manifest package/package.xml --target-username $USERNAME --postdestructivechanges package/destructiveChanges.xml --wait 0 --json > validation.json
+    cat deployment.json
+    deploymentStatus=$(jq '.status' deployment.json)
+    deploymentId=$(jq '.result.id' deployment.json | sed 's/"//g')
+    echo "Validation Status: $deploymentStatus --------- DeploymentId: $deploymentId"
+    if [ "$deploymentStatus" != 0 ];
+    then
+        errorMessage=$(jq '.result.message' validation.json)
+        echo "*****Error***** Deploymnet Failed ..... Error Message: $errorMessage"
+        exit 1
+    else
+        echo "Deployment Started with Deployment Id: $deploymentId"
+        echo "DEPLOYMENTID=$deploymentId" >> $GITHUB_ENV
+    fi
+else
+    echo "No Source backed components present in the package.xml. So, Deployment failed to start"
+fi
